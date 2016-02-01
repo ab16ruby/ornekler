a8d455b9a161b03cbd5d0c764cca338d52b5888a
Umurcan Görür <mail@ugorur.com>
2016-02-01 16:28:13 +0200

    Menuler sınıfı ayrı dosyaya alındı

---
+++ aydin/menuler.rb
@@ -0,0 +1,36 @@
+class Menuler
+	class << self
+		def ana
+			puts "Lütfen menüden bir seçenek seçiniz:"
+
+			puts "1) Adres Defterinizi Listeleyiniz"
+			puts "2) Yeni Kayıt Ekleyin"
+			puts "3) Adında Arayın"
+			puts "4) Soyadında Arayın"
+
+			puts "---"
+			puts "Çıkmak için E yazın"
+			puts "---"
+			print "Seçiminiz: "
+
+			gets.chomp.upcase
+		end
+
+		def liste
+			puts "---"
+			puts "Çıkmak için E, Ana Menu için G yazınız"
+			puts "---"
+			print "Seçiminiz: "
+
+			gets.chomp.upcase
+		end
+
+		def liste? secim
+			["E", "G"].include? secim
+		end
+
+		def ana? secim
+			["E", *"1".."4"].include? secim
+		end
+	end
+end
