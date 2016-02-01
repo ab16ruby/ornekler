7488acdffdc5d6c0d2e7d8728e20e3b6a3f795c0
Umurcan Görür <mail@ugorur.com>
2016-02-01 11:29:25 +0200

    Kişi sınıfı yazıldı

--- aydin/kisi.rb
+++ aydin/kisi.rb
@@ -4,20 +4,12 @@ require "yaml"
 class Kisi
 	attr_accessor :adi, :soyadi, :sehri, :dtarih, :telefon, :eposta
 
-	@@toplam_kisi = 0
-
 	def initialize (adi, soyadi, **params)
 		@adi, @soyadi = adi, soyadi
 		@sehri = params[:sehri] if params.include? :sehri
 		@dtarih = params[:dtarih] if params.include? :dtarih
 		@telefon = params[:telefon] if params.include? :telefon
 		@eposta = params[:eposta] if params.include? :eposta
-
-		@@toplam_kisi += 1
-	end
-
-	def self.toplam_kisi_sayisi
-		@@toplam_kisi
 	end
 
 	def self.girdi(veri)
@@ -38,39 +30,4 @@ class Kisi
 	def cikti
 		block_given? ? yield(@adi, @soyadi, @sehri, @dtarih, @telefon, @eposta) : [@adi,@soyadi,@sehri,@dtarih,@telefon,@eposta].join(',')
 	end
-
-	protected
-
-	def dtarih_valid?
-		"Deneme"
-	end
 end
-
-
-class Ogrenci < Kisi
-
-	def notlar
-		puts dtarih_valid?
-	end
-
-end
-
-
-umur = Ogrenci.new("Umurcan", "Görür", telefon: "05384319043")
-
-# puts umur.adi
-# puts umur.soyadi
-# puts umur.cikti
-# puts umur.cikti { |*veriler| veriler.to_json }
-# puts umur.cikti { |*veriler| veriler.to_yaml }
-
-umur.notlar
-
-basak = Kisi.girdi("Başak,Çakmaktepe,Ankara,30/04/1993,05225551111,basakcakmaktepe@gmail.com")
-puts basak.adi
-puts basak.telefon
-puts basak.cikti
-
-#puts basak.dtarih_valid?
-
-puts "Toplam #{Kisi.toplam_kisi_sayisi} kişi oluşturuldu."
